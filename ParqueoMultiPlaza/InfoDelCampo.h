89e819f6055a7f8c0d7a9d5cecfbad6a05771aec
charly <carlosgarita3110@gmail.com>
2022-09-17 15:32:39 -0600

    InfoDelCampo class definition

---
+++ ParqueoMultiPlaza/InfoDelCampo.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Vehiculo.h"
+
+class InfoDelCampo {
+private:
+    int numeroMaximoDeVehiculos; //tamano del array
+    int numeroDeCarrosRegistrados; //cantidad //carros que se han registrado en ese dia // maximo 10
+    char estadoCampo; //O, M, L
+    double dineroGeneradoPorELCampo;
+    Vehiculo** registroDeVehiculos; //array
+public:
+    InfoDelCampo(int numeroMaximoDeVehiculos = 10, char estadoCampo = 'L', int numeroDeCarrosRegistrados = 0);
+    virtual ~InfoDelCampo();
+    //getters
+    int getNumeroMaximoDeVehiculos();
+    int getNumeroDeCarrosRegistrados();
+    char getEstadoCampo();
+    double getDineroGeneradoPorELCampo();
+    Vehiculo** getRegistroDeVehiculos();
+    //setters
+    //no se puede cambiar el numero maximo de vehiculos
+    // void setNumeroMaximoDeVehiculos(int);
+    // void setNumeroDeCarrosRegistrados(int);
+    void setEstadoCampo(char);
+    void setDineroGeneradoPorELCampo(double);
+    void setRegistroDeVehiculos(Vehiculo**);
+    bool ingresarVehiculo(Vehiculo* vehiculo);
+    string toString();
+};
