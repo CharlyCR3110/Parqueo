e5aba54083eb49b5172e60d434ffad9016802d7c
charly <carlosgarita3110@gmail.com>
2022-09-17 15:10:28 -0600

    Cobro class definition

---
+++ ParqueoMultiPlaza/Cobro.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Hora.h"
+
+class Cobro {
+private:
+	Hora* horaDeEntrada;
+	Hora* horaDeSalida;
+	double totalPagar;
+public:
+	//Constructor/Destructor
+	Cobro(Hora* = NULL, Hora* = NULL);
+	virtual ~Cobro();
+	//getters
+	Hora* getHoraDeEntrada();
+	Hora* getHoraDeSalida();
+	double getTotalPagar();
+	//setters
+	void setHoraDeEntrada(Hora*);
+	void setHoraDeSalida(Hora*);
+	void setTotalPagar(double);
+	//metodos
+	string toString();
+};
