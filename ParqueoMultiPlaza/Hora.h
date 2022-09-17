a963e9b3c514a08e6bb72b997f519f74ba875f36
charly <carlosgarita3110@gmail.com>
2022-09-17 15:07:07 -0600

    Hora class definition

---
+++ ParqueoMultiPlaza/Hora.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Hora {
+private:
+	int horas;
+	int minutos;
+	int segundos;
+public:
+	Hora(int = 0, int = 0, int = 0);
+	virtual ~Hora();
+	//getters
+	int getHora();
+	int getMinutos();
+	int getSegundos();
+	//setters
+	void setHoras(int);
+	void setMinutos(int);
+	void setSegundos(int);
+	//metodos
+	string toString();
+};
