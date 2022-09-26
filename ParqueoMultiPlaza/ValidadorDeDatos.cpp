f139f7d3bdbdd87f4ebd881974de4da86ec39553
charly <carlosgarita3110@gmail.com>
2022-09-25 18:16:54 -0600

    Add: Function in charge of verifying that the time data entered is in the correct format

---
+++ ParqueoMultiPlaza/ValidadorDeDatos.cpp
@@ -0,0 +1,12 @@
+#include "ValidadorDeDatos.h"
+
+bool validarHora(int hora, int minutos, int segundos) {
+    if (hora >= 0 && hora <= 23) {
+        if (minutos >= 0 && minutos <= 59) {
+            if (segundos >= 0 && segundos <= 59) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
---
+++ ParqueoMultiPlaza/ValidadorDeDatos.h
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+
+bool validarHora(int, int, int);
