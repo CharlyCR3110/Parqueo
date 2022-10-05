a9f3bdb49c4e6622f308f4e12497697566858647
charly <carlosgarita3110@gmail.com>
2022-10-05 13:30:52 -0600

    Comms: Add comments

--- ParqueoMultiPlaza/PausaYContinuar.cpp
+++ ParqueoMultiPlaza/PausaYContinuar.cpp
@@ -1,5 +1,7 @@
 #include "PausaYContinuar.h"
 
+
+//estos 2 comandos se repetian mucho en el codigo, entonces decidi hacer una funcion para que no se repitiera tanto codigo
 void pausaYContinuar()
 {
 	std::system("pause");
