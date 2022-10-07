88a3c7585b2644a8429831850c0363796e7596f7
charly <carlosgarita3110@gmail.com>
2022-10-07 17:32:18 -0600

    delete

--- ParqueoMultiPlaza/PausaYContinuar.cpp
+++
@@ -1,9 +0,0 @@
-#include "PausaYContinuar.h"
-
-
-//estos 2 comandos se repetian mucho en el codigo, entonces decidi hacer una funcion para que no se repitiera tanto codigo
-void pausaYContinuar()
-{
-	std::system("pause");
-	std::system("cls");
-}
