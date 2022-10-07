f24e9d123afb80144b18fe5b087f1e624385a7f3
charly <carlosgarita3110@gmail.com>
2022-10-07 17:56:02 -0600

    Add: method in charge of pausing

---
+++ ParqueoMultiPlaza/funcionesVarias.cpp
@@ -0,0 +1,11 @@
+#include "funcionesVarias.h"
+
+
+//estos comandos se repetian mucho en el codigo, 
+//entonces decidi hacer una funcion para que no se repitiera tanto codigo
+
+void pausaYContinuar()
+{
+	std::system("pause");
+	std::system("cls");
+}
